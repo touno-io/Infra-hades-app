e2bca4bdce373d55b11cce346ed80faff878ddc1
Kananek Thongkam <info.dvgamer@gmail.com>
2022-10-19 14:53:47 +0700

    frontend update scss

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
-// eslint-disable-next-line
-import init from 'hades-app';
+// @ts-ignore
+import init from 'hades-app'
 
 import { createApp } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import App from './App.vue'
+
 import './samples/node-api'
+import 'bootstrap/scss/bootstrap-grid.scss'
+import './assets/scss/global.scss'
+import './fontAwesome.ts'
+
+postMessage({ payload: 'removeLoading' }, '*')
+const app = createApp(App).component('fa', FontAwesomeIcon)
 
 init().then(() => {
-  return createApp(App)
-  .mount('#app')
-  .$nextTick(() => {
-    postMessage({ payload: 'removeLoading' }, '*')
-  })
+  return app.mount('#app')
 })
 
